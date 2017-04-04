462f0c4441d5614d2642f8bae639c857fbf1f9d0
Taps Murove <taps.murove@gmail.com>
2017-04-04 15:10:29 +0200

    removed todo files
    - removed todo files
    - created header and notebook containers

---
+++ src/containers/HeaderContainer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderContainer = () => (
+    <nav className="navbar navbar-default">
+        <div className="container-fluid">
+            <div className="nav-header">
+                <a className="navbar-brand" href="#">WritePad 1</a>
+            </div>
+        </div>
+    </nav>
+    );
+
+export default HeaderContainer;
---
+++ src/containers/NotebookContainer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const NotebookContainer = () => (
+    <div>
+       <p className="lead">Please Login to Continue</p>
+    </div>
+    );
+
+export default NotebookContainer;

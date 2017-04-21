af460d866077a3da345f4aea46fae2b8b93c10db
Taps Murove <taps.murove@gmail.com>
2017-04-21 14:38:35 +0200

    user can delete a project

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,45 +1,10 @@
 import React from 'react';
-import { Layout, Table, Icon, Card, Button } from 'antd';
+import { Layout, Table, Card, Button, Popconfirm } from 'antd';
 import { connect } from 'react-redux';
 import { firebase, helpers } from 'redux-react-firebase';
 import _ from 'lodash';
 
 const { pathToJS, dataToJS } = helpers;
-
-const columns = [{
-  title: 'Project Name',
-  dataIndex: 'name',
-  key: 'name',
-  render: text => <a href="#">{text}</a>,
-}, {
-  title: 'Owner',
-  dataIndex: 'owner',
-  key: 'owner',
-}, {
-  title: 'Actions',
-  key: 'actions',
-  render: (text, record) => (
-    <span>
-      <a href="#"><Icon type="user-add" /></a>
-      <span className="ant-divider" />
-      <a href="#"><Icon type="delete" /></a>
-    </span>
-  ),
-}];
-
-// const projectData = [{
-//   key: '1',
-//   name: 'A Tale of Two Cities',
-//   owner: 'Charles Dickens',
-// }, {
-//   key: '2',
-//   name: 'Warbreaker',
-//   owner: 'Brandon Sanderson',
-// }, {
-//   key: '3',
-//   name: 'The People',
-//   owner: 'Taps Murove',
-// }];
 
 const cardStyle = {
     margin: '0 auto', 
@@ -63,26 +28,75 @@ const cardStyle = {
         })}
 )
 class Projects extends React.Component {
+    constructor(props) {
+        super(props);
+
+        this.columns = [{
+                        title: 'Project Name',
+                        dataIndex: 'name',
+                        key: 'name',
+                        render: text => <a href="#">{text}</a>,
+                        }, {
+                        title: 'Owner',
+                        dataIndex: 'owner',
+                        key: 'owner',
+                        }, {
+                        title: 'Actions',
+                        key: 'actions',
+                        render: (text, record, index) => (
+                            <Popconfirm title="Are you sure?" onConfirm={() => this.onDelete(record.key)} okText='Confirm' cancelText='Cancel'>
+                                <a href="#">Delete</a>
+                            </Popconfirm>
+                        ),
+                    }];
+        
+
+        this.state = {
+            dataSource: undefined,
+        }
+    };
+
+    handleNewProject() {
+        const { firebase, auth } = this.props;
+        firebase.push('/projects', {owner: auth.uid , name: 'Untitled', content: {block: ['this', 'is', 'that']}});
+    };
+
+    onDelete(key) {
+        const { firebase } = this.props;
+        firebase.remove(`/projects/${key}`);
+    };
+
+    componentWillUpdate(nextProps, nextState) {
+        if (nextProps !== this.props) {
+            const { projects } = nextProps;
+
+            if (projects) {
+                const projectList = _.transform(projects, (result, value, key) => {
+                    result.push({ key: key, name: value.name, owner: value.owner })
+                }, []);
+
+                this.setState({
+                    dataSource: projectList,
+                });
+            }
+        } 
+    };
+
     render () {
 
-        const { firebase, auth, projects } = this.props;
-
-
-        const handleNewProject = () => {
-            firebase.push('/projects', {owner: auth.uid , name: 'Untitled', content: {block: ['this', 'is', 'that']}})
-        };
-
-        const projectList = () => {
-            const list = _.values(projects);
-            return list;
-        }
-        console.log(projectList())
-
+        const { dataSource } = this.state;
+        const columns = this.columns;
+         
         return (
             <Layout style={{ marginTop: '80px', width: '100%', padding: '0', bottom: '50px', height: 'auto' }}>
                 <Card title="Projects" bordered={false} style={cardStyle} >
-                    <Button style={{marginBottom: '15px'}} onClick={handleNewProject}>New Project</Button>
-                    <Table style={{height: '200px'}} pagination={{ pageSize: 8 }} columns={columns} dataSource={projectList()} />
+                    <Button style={{marginBottom: '15px'}} onClick={() => this.handleNewProject()}>New Project</Button>
+                    <Table
+                    locale={{emptyText: 'Create a new project'}} 
+                    style={{height: '200px'}} 
+                    pagination={{ pageSize: 8 }} 
+                    columns={columns} 
+                    dataSource={dataSource} />
                 </Card>
             </Layout>)
     };
